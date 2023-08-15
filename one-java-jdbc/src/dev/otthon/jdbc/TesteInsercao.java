cc9f2799e937ca0fc2f53e3b50ccf1e580be62ea
Otthon Leão <54039360+otthonleao@users.noreply.github.com>
2023-08-14 23:44:31 -0400

    feat: classe para inserção pelo id

---
+++ one-java-jdbc/src/dev/otthon/jdbc/TesteInsercao.java
@@ -0,0 +1,28 @@
+package dev.otthon.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TesteInsercao {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
+		Statement stm = connection.createStatement();
+		stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Mouse sem fio')",
+			Statement.RETURN_GENERATED_KEYS);
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		
+		while(rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println(id);
+		}
+		
+		connection.close();
+	}
+}
