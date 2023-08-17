022c79b39bb2ec3cb5f5a13ed07a563b40a439dc
Otthon Leão <54039360+otthonleao@users.noreply.github.com>
2023-08-16 22:30:24 -0400

    refactor: criando o pool de conexoes do datasource

--- one-java-jdbc/src/dev/otthon/jdbc/ConnectionFactory.java
+++ one-java-jdbc/src/dev/otthon/jdbc/ConnectionFactory.java
@@ -1,14 +1,29 @@
 package dev.otthon.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class ConnectionFactory {
 	
-	public Connection recuperarConexao() throws SQLException {
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/one_loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("");
 		
-		return DriverManager.getConnection
-				("jdbc:mysql://localhost/one_loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "");
+		/** NÚMERO MÁXIMO DE CONEXÕES */
+		comboPooledDataSource.setMaxPoolSize(15);
+		
+		this.dataSource = comboPooledDataSource;
+		
+	}
+	
+	public Connection recuperarConexao() throws SQLException {
+		return this.dataSource.getConnection();
 	}
 }
+
